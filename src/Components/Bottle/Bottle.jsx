aaddda6dbcab79ae75eebb3239c24ccf7a8f867f
mahamudul hasan seber <sebermh@gmail.com>
2026-02-27 01:13:54 +0600

    JSON data Successfully Loaded

---
+++ src/Components/Bottle/Bottle.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Bottle = ({ bottle }) => {
+  //   console.log(bottle);
+  const { name, img, price, color, brand, description, model } = bottle;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={img} alt={name} />
+      <p>Price: ${price}</p>
+      <p>Color: {color}</p>
+      <p>Brand: {brand}</p>
+      <p>Description: {description}</p>
+      <p>Model: {model}</p>
+      <button className="btn">Buy Now</button>
+    </div>
+  );
+};
+
+export default Bottle;
--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,11 +1,15 @@
 import React, { use } from "react";
+import Bottle from "../Bottle/Bottle";
 
 const Bottles = ({ bottlesPromise }) => {
   const bottles = use(bottlesPromise);
   console.log(bottles);
   return (
     <div>
-      <h1>Bottles</h1>
+      <h1>Bottles : {bottles.length}</h1>
+      {bottles.map((bottle) => (
+        <Bottle key={bottle.id} bottle={bottle} />
+      ))}
     </div>
   );
 };
